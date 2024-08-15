27abfef39f9783884c91a349c6573f831299f17f
raphael-ming <raphael.jeanirvin@gmail.com>
2024-08-15 22:28:19 +0700

    tambahan pertanyaan fedlim

--- topicBased/Advanced Iteration/passBreakContinue.py
+++ topicBased/Advanced Iteration/passBreakContinue.py
@@ -24,6 +24,7 @@ def continueFunc():
         if i%2 == 0:
             print (i)
         else:
+            i+=1
             continue #-> Continue = Skip the rest of the loop, immediately start the loop from top!
         print (f'NUMBER {i} IS PROCESSED!')
         i += 1
@@ -40,3 +41,22 @@ print ("")
 
 print ("THIS IS CONTINUE")
 continueFunc()
+
+def pass_break_continue_quiz():
+    numbers = [12, 75, 150, 180, 145, 525, 50]
+    #Harus bisa dibagi 5
+    #Kalo lebih besar dari 150, skip, move to next number
+    #Kalo lebih dari 500, stop the loop
+
+    for x in numbers:
+        if x%5 == 0:
+            if x > 500:
+                break
+            elif x > 150:
+                continue
+            else:
+                print(x)
+        else:
+            pass
+
+pass_break_continue_quiz()
